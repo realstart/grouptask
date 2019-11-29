2f08da0bbbf02bcbc9d6233fe69dbd65019a3ebb
Higherally <realstart>
2019-11-30 06:06:44 +0800

    updated

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?
   $lang = "en";
 
-  if (strpos($_SERVER['SERVER_NAME'], 'mt') !== false || strpos($_SERVER['SERVER_NAME'], 'fr') !== false) {
-      $lang = "fr";
+  if (strpos($_SERVER['SERVER_NAME'], 'mt') !== false) {
+      $lang = "en";
+  }
+  elseif (strpos($_SERVER['SERVER_NAME'], 'fr') !== false) {
+    $lang = "fr";
   }
 
   $devMode = false;
